dd62c598d859a5cd2f8c0a6a4aee984701680ef0
sreekant2x <sreekantsaha@gmaii.com>
2024-05-12 23:36:18 +0530

    added login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import ProfilePage from "./routes/profilePage/ProfilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
 
 function App() {
   const router = createBrowserRouter([
@@ -27,6 +29,14 @@ function App() {
           path: "/profile",
           element: <ProfilePage />
         },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
+        }
       ]
     }
   ]);
